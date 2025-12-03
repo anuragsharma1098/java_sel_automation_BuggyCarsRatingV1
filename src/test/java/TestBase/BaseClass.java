9c454a7575e7b0d52febaf5c577bd929d67f20b5
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-12-03 15:20:45 +0530

    parallel Test case and config file

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -1,26 +1,59 @@
 package TestBase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	public WebDriver driver;
 	public Logger logger; // Log4j
+	public Properties configProp;
 
 	@BeforeClass
-	public void setup() {
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String br) throws FileNotFoundException {
+		FileReader fileReader = new FileReader("./src/test/resources/config.properties");
+		configProp = new Properties();
+		try {
+			configProp.load(fileReader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		logger = LogManager.getLogger(this.getClass());
-		driver = new ChromeDriver();
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "safari":
+			driver = new SafariDriver();
+			break;
+		default:
+			System.out.println("Please provide a valid browser name");
+			return;// Exit the method if the browser name is invalid
+		}
+		// driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://buggy.justtestit.org/");
+		driver.get(configProp.getProperty("baseURL"));
 		driver.manage().window().maximize();
 	}
 
@@ -41,7 +74,7 @@ public class BaseClass {
 
 	public String randomAlphaNumeric() {
 		String str = RandomStringUtils.randomAlphabetic(4);
-		String num = RandomStringUtils.randomNumeric(3);
+		String num = RandomStringUtils.randomNumeric(4);
 		return str + "@" + num;
 	}
 }
