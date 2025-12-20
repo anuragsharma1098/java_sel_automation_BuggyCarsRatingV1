d1bd0b458f4cee19647588409d496667eb16fba5
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-12-20 21:49:26 +0530

    Selenium Grid

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -13,11 +15,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -30,7 +35,7 @@ public class BaseClass {
 
 	@BeforeClass(groups = { "master", "sanity", "regression", "dadadriven" })
 	@Parameters({ "os", "browser" })
-	public void setup(String os, String br) throws FileNotFoundException {
+	public void setup(String os, String br) throws FileNotFoundException, MalformedURLException {
 		FileReader fileReader = new FileReader("./src/test/resources/config.properties");
 		configProp = new Properties();
 		try {
@@ -39,22 +44,64 @@ public class BaseClass {
 			e.printStackTrace();
 		}
 		logger = LogManager.getLogger(this.getClass());
-		switch (br.toLowerCase()) {
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-		case "safari":
-			driver = new SafariDriver();
-			break;
-		default:
-			System.out.println("Please provide a valid browser name");
-			return;// Exit the method if the browser name is invalid
+		if (configProp.getProperty("execution_env").equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+
+			// os
+			if (os.equalsIgnoreCase("windows")) {
+
+				capabilities.setPlatform(Platform.WIN11);
+				// capabilities.setBrowserName()
+			} else if (os.equalsIgnoreCase("mac")) {
+
+				capabilities.setPlatform(Platform.MAC);
+				// capabilities.setBrowserName()
+			} else {
+
+				System.out.println("No matching OS");
+				return;
+			}
+
+			// browser
+			switch (br.toLowerCase()) {
+			case "chrome":
+				capabilities.setBrowserName("chrome");
+				break;
+			case "edge":
+				capabilities.setBrowserName("MicrosoftEdge");
+				break;
+			case "firefox":
+				capabilities.setBrowserName("firefox");
+				break;
+			case "safari":
+				capabilities.setBrowserName("safari");
+				break;
+			default:
+				System.out.println("No matching browser");
+				return;// Exit the method if the browser name is invalid
+			}
+
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+		}
+		if (configProp.getProperty("execution_env").equalsIgnoreCase("local")) {
+
+			switch (br.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
+			case "safari":
+				driver = new SafariDriver();
+				break;
+			default:
+				System.out.println("Please provide a valid browser name");
+				return;// Exit the method if the browser name is invalid
+			}
 		}
 		// driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
