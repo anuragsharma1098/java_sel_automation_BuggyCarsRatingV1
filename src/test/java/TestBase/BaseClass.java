a043255b63a19f2866974cc23b2d55c6859fce2c
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-01-25 20:48:46 +0530

    update

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -28,6 +28,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass {
 	public static WebDriver driver;
 	public Logger logger; // Log4j
@@ -68,43 +70,46 @@ public class BaseClass {
 
 			// browser
 			switch (br.toLowerCase()) {
-			case "chrome":
-				capabilities.setBrowserName("chrome");
-				break;
-			case "edge":
-				capabilities.setBrowserName("MicrosoftEdge");
-				break;
-			case "firefox":
-				capabilities.setBrowserName("firefox");
-				break;
-			case "safari":
-				capabilities.setBrowserName("safari");
-				break;
-			default:
-				System.out.println("No matching browser");
-				return;// Exit the method if the browser name is invalid
+				case "chrome":
+					capabilities.setBrowserName("chrome");
+					break;
+				case "edge":
+					capabilities.setBrowserName("MicrosoftEdge");
+					break;
+				case "firefox":
+					capabilities.setBrowserName("firefox");
+					break;
+				case "safari":
+					capabilities.setBrowserName("safari");
+					break;
+				default:
+					System.out.println("No matching browser");
+					return;// Exit the method if the browser name is invalid
 			}
 
 			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
 		}
 		if (configProp.getProperty("execution_env").equalsIgnoreCase("local")) {
-
 			switch (br.toLowerCase()) {
-			case "chrome":
-				driver = new ChromeDriver();
-				break;
-			case "edge":
-				driver = new EdgeDriver();
-				break;
-			case "firefox":
-				driver = new FirefoxDriver();
-				break;
-			case "safari":
-				driver = new SafariDriver();
-				break;
-			default:
-				System.out.println("Please provide a valid browser name");
-				return;// Exit the method if the browser name is invalid
+				case "chrome":
+					WebDriverManager.chromedriver().setup();
+					driver = new ChromeDriver();
+					break;
+				case "edge":
+					WebDriverManager.edgedriver().setup();
+					driver = new EdgeDriver();
+					break;
+				case "firefox":
+					WebDriverManager.firefoxdriver().setup();
+					driver = new FirefoxDriver();
+					break;
+				case "safari":
+					WebDriverManager.safaridriver().setup();
+					driver = new SafariDriver();
+					break;
+				default:
+					System.out.println("Please provide a valid browser name");
+					return;// Exit the method if the browser name is invalid
 			}
 		}
 		// driver = new ChromeDriver();
